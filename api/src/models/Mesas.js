0c4254cfc0c838dfd899097863b0832c65260480
Mariano Rebord <53273967+mrptato@users.noreply.github.com>
2021-11-09 23:15:51 -0300

    Delete api directory

--- api/src/models/Mesas.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const MesaSchema = new Schema({
-  tableNumber: { type: Number, required: true },
-  isOccupated: { type: Boolean, default: false },
-});
-
-module.exports = mongoose.model("Mesa", MesaSchema);
--- api/src/models/ProductsType.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-
-const ProductsTypeSchema = new Schema({
-  name: { type: String, required: true },
-});
-
-module.exports = mongoose.model("ProductsType", ProductsTypeSchema);
