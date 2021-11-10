9ca29f9107e589b1c9a321d56afc41375659fa7b
Mariano Rebord <53273967+mrptato@users.noreply.github.com>
2021-11-09 23:16:09 -0300

    Delete client directory

--- client/src/components/Home/Home.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import Dashboard from "./views/Dashboard";
-import Error403 from "./views/Error403";
-
-export default function Home() {
-  const token = useSelector((state) => state.userToken);
-  return <div>{token ? <Dashboard /> : <Error403 />}</div>;
-}
